33a8551b1f1a0082dba2326e4ca8383c817f066c
Uttam Patel <86312108+Uttampatel1@users.noreply.github.com>
2024-01-18 13:08:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ def get_molecular_formula_and_weight(smiles_input):
         return None, None
     
 # @st.cache_resource(experimental_allow_widgets=True)
-@st.cache_data
-# @st.cache_data(ttl=3600)
+# @st.cache_data
+@st.cache_data(ttl=3600)
 def display_img(smiles_list):
     image_id = 0
     data_for_csv = []
