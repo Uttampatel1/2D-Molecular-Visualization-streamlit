af72f0b4d47f61a4136cf64f94b212a181553c0f
Uttam Patel <86312108+Uttampatel1@users.noreply.github.com>
2024-02-26 18:37:09 +0530

    Update PIC50 and IC50.py

--- pages/PIC50 and IC50.py
+++ pages/PIC50 and IC50.py
@@ -1,30 +1,43 @@
 import streamlit as st
-import math
+import numpy as np
 
-def pic50_to_ic50(pic50):
-    ic50 = 10 ** (-pic50)
+def pic50_to_ic50(pic50, unit):
+    if unit == "millimeter":
+        ic50 = np.power(10, -pic50)
+    elif unit == "micrometer":
+        ic50 = np.power(10, -6-pic50)
+    elif unit == "nanometer":
+        ic50 = np.power(10, -9-pic50)
     return ic50
 
-def ic50_to_pic50(ic50):
-    pic50 = -math.log10(ic50)
+def ic50_to_pic50(ic50, unit):
+    pic50 = None
+    if unit == "millimeter":
+        pic50 = -np.log10(ic50)
+    elif unit == "micrometer":
+        pic50 = -np.log10(ic50 * 1e6)
+    elif unit == "nanometer":
+        pic50 = -np.log10(ic50 * 1e9)
     return pic50
 
 def main():
-    st.title("🔢 PIC50 <-> IC50 Converter 🔢")
-    st.write("This app converts between PIC50 and IC50 values.")
+    st.title("pIC50 to IC50 Converter")
 
-    conversion_mode = st.radio("Select conversion mode:", ("PIC50 to IC50", "IC50 to PIC50"))
+    conversion_type = st.radio("Select conversion type:", ("pIC50 to IC50", "IC50 to pIC50"))
 
-    if conversion_mode == "PIC50 to IC50":
-        pic50_input = st.number_input("Enter PIC50 value:", min_value=0.0, step=0.1, format="%.1f")
+    if conversion_type == "pIC50 to IC50":
+        pic50 = st.number_input("Enter pIC50 value:", min_value=0.0, step=0.1)
+        unit = st.selectbox("Select unit of concentration:", ("millimeter", "micrometer", "nanometer"))
         if st.button("Convert"):
-            ic50_result = pic50_to_ic50(pic50_input)
-            st.write(f"The corresponding IC50 value is: {ic50_result:.5f}")
-    elif conversion_mode == "IC50 to PIC50":
-        ic50_input = st.number_input("Enter IC50 value:", min_value=0.0, step=0.0001, format="%.4f")
+            ic50 = pic50_to_ic50(pic50, unit)
+            st.success(f"IC50 value: {ic50:.6f} {unit}")
+
+    elif conversion_type == "IC50 to pIC50":
+        ic50 = st.number_input("Enter IC50 value:", min_value=0.0, step=0.000001)
+        unit = st.selectbox("Select unit of concentration:", ("millimeter", "micrometer", "nanometer"))
         if st.button("Convert"):
-            pic50_result = ic50_to_pic50(ic50_input)
-            st.write(f"The corresponding PIC50 value is: {pic50_result:.2f}")
+            pic50 = ic50_to_pic50(ic50, unit)
+            st.success(f"pIC50 value: {pic50:.2f}")
 
 if __name__ == "__main__":
     main()
